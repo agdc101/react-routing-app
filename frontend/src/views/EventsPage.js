b32d55215d941888a0453dda1e0480cbb1a81ac9
agdc101 <agdc101@gmail.com>
2023-12-21 18:33:02 +0000

    feat: events data is deferred using router defer

--- frontend/src/views/EventsPage.js
+++ frontend/src/views/EventsPage.js
@@ -1,22 +1,34 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
-  const fetchedEvents = data.events;
+  const { events } = useLoaderData();
 
-  return <EventsList events={fetchedEvents} />
+  return (
+    <Suspense fallback={<p>Loading...</p>} >
+      <Await resolve={events}>
+            {(loadedEvents) => <EventsList events={loadedEvents} /> }
+      </Await>
+    </Suspense>
+  ) 
+
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function fetchEvents() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     return json({ message: 'Something went wrong!' }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({ id: 'events', data: fetchEvents()})
 }
